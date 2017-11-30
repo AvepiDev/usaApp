5c9a4bd72c3fdc4b57ca1f06fa6fc27b6d7d9cc7
AvepiDev <mario_dev@avepistudios.com>
2017-11-29 19:32:49 -0600

    User-Login

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,3 @@
 export const environment = {
   production: true,
-  firebase : {
-    apiKey: "AIzaSyDr7v9cFWb9kM38DiHggv4adIxrrhA3RoA",
-    authDomain: "angulardb-fbfcd.firebaseapp.com",
-    databaseURL: "https://angulardb-fbfcd.firebaseio.com",
-    projectId: "angulardb-fbfcd",
-    storageBucket: "angulardb-fbfcd.appspot.com",
-    messagingSenderId: "286151433358"
-  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,5 @@
 export const environment = {
   production: false,
   firebase : {
-    apiKey: "AIzaSyDr7v9cFWb9kM38DiHggv4adIxrrhA3RoA",
-    authDomain: "angulardb-fbfcd.firebaseapp.com",
-    databaseURL: "https://angulardb-fbfcd.firebaseio.com",
-    projectId: "angulardb-fbfcd",
-    storageBucket: "angulardb-fbfcd.appspot.com",
-    messagingSenderId: "286151433358"
   }
 };
